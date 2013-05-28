d705242e5d29c34d8bb9305a23a4ab8fdbff4703
David Orrick <ddorrick@aol.com>
2013-05-28 16:20:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DIBS3::Application.config.secret_token = 'e4918b6ddee4aae2d496d0cf64688ce912f4e3fa7684caaf59593d6602b44906580ba59f243bd92139232b8a55907434a39f2bfa0161decfeaf15363f0d926d6'
